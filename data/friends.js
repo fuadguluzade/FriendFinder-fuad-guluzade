423df4747e884f777a44aa36aa6969ce8b556b7a
fuadguluzade <44554892+fuadguluzade@users.noreply.github.com>
2019-10-07 00:02:28 -0700

    changed images

--- data/friends.js
+++ data/friends.js
@@ -1,11 +1,11 @@
 let friends = [{
     name: 'Vladimir Putin',
-    photo: 'https://thenypost.files.wordpress.com/2019/09/russia-putin-trump-calls.jpg?quality=90&strip=all&w=618&h=410&crop=1',
+    photo: 'https://www.nzherald.co.nz/resizer/lMDQo49XHmHAk656n5O7Fw4sVUY=/360x384/filters:quality(70)/arc-anglerfish-syd-prod-nzme.s3.amazonaws.com/public/U2K5YOOI4NHPXLT25PB4L7O6UA.jpg',
     scores: [4, 3, 5, 2, 1, 3, 2, 5, 5, 1]
 },
 {
     name: 'Donald Trump',
-    photo: 'https://static01.nyt.com/images/2019/10/22/opinion/22leonardtSub/merlin_160248591_2883e31a-1b08-487c-9dac-88b37ec03f0c-articleLarge.jpg?quality=75&auto=webp&disable=upscale',
+    photo: 'https://pbs.twimg.com/profile_images/691046991325827072/oQv290pS_400x400.jpg',
     scores: [5, 4, 4, 1, 1, 3, 5, 1, 4, 3]
 },
 {
